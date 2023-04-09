faaf12810afdef348c26840963a58ed8a5a84d19
unknown <maurosanchez@gmail.com>
2023-04-09 17:41:49 -0300

    estructura_REACT

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../assets/css/App.css';
+
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
+function App() {
+  return (
+    <React.StrictMode>
+    
+      <Header/>
+       <Body/>
+       <Footer/>
+
+  
+    </React.StrictMode>
+  );
+}
+
+export default App;
