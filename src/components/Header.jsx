225092b72bbd57203256cf01d9a1e47db3dc6a5e
FrancoSanchezCamps <francosanchezcamps.100@gmail.com>
2023-04-10 00:54:00 -0300

    ancords

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,31 @@ function Header() {
     <section> 
       
     <div>Header
-    </div>
-    <div>
+    </div><br />
+        <div>
       <button className='button-burger'>
+      </button ><br />
+            <div>
             <i className="fa-sharp fa-solid fa-bars lista-hamburguesa"> Lista hamburguesa </i>
-        </button ></div>
+            </div><br />
+           <div>
+            <input placeholder="Buscar" type="search" className="input" name="search"/>
+            </div><br />
+            <div>
+              <li><a href="/product/create">Crear Producto</a></li>
+              <li><a href="/product/list">Todos los Productos</a></li>
+            <br />
+            </div>
+            <div>
+            <li><a href="/users/logout">Salir</a></li>
+            <li><a href="/users/register">Registrate</a></li>
+            <li><a href="/users/login">Ingresá</a></li>
+            <li><a href="/product/productCart">Carrito</a></li>
+            </div>
+        </div>
+
     </section>
+    
     
     
   )
