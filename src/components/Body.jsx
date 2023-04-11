26fdb9cc0150a77ed8b363a82996132a16006abe
FrancoSanchezCamps <francosanchezcamps.100@gmail.com>
2023-04-11 14:10:03 -0300

    body.css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function Body() {
   return (
-    <div>Body</div>
+    <body>
+      
+    <div className="content-servicios">
+        <div className="content-servicios__div">           
+            <i className="fa fa-users ico"></i>
+            <h2>Sobre IKA</h2>
+            
+            <p className="parrafo">Dinámica, profesionalismo y tecnología a su servicio. Orientados en la atención al cliente y en su satisfacción total.</p>
+            <a href="#"><i className="fa fa-angle-right"></i> Conozcanos</a>
+        </div>
+        <div className="content-servicios__div">
+            <i className="fa fa-car ico"></i>
+            <h2>IKA Auto</h2>
+       
+            <p className="parrafo">Ika Plan te permite acceder a tu Auto 0KM totalmente en cuotas sin interés.</p>
+            <a href="#"><i className="fa fa-angle-right"></i> Ver modelos</a>
+        </div>
+    </div>
+    </body>
   )
 }
 
