433d2a934841f0dacb008b187e5b8b621e75835d
FrancoSanchezCamps <francosanchezcamps.100@gmail.com>
2023-04-15 22:05:29 -0300

    apiUserReact

--- src/components/ApiUsers.jsx
+++ src/components/ApiUsers.jsx
@@ -1,27 +1,47 @@
 import React, {Component} from 'react';
 
-class ApiProducts extends Component {
-    state ={
-        usersList: [],
+class ApiUsers extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            ApiUsuario: []
+        }
+    }
+    apiCall(url , consecuencia){
+        fetch(url)
+            .then(response => response.json())
+            .then(data => consecuencia(data))
+            .catch( e => console.log(e))
     }
     componentDidMount(){
-        fetch("")
-        .then((r) => r.json())
-        .then((respuesta) => {
-            this.setState({ usersList: respuesta.data })
-        })
-        .catch((error) => console.log(error));
+        console.log("me monté")
+        this.apiCall("http://localhost:3001/api/users",this.mostrarUsuario)
+    }
+    mostrarUsuario = (data) =>{
+        console.log(data)
+        this.setState(
+            {
+                ApiUsuario: data.data.map((imprimir ,i) => imprimir = <p key={imprimir.first_name + i}></p>
+                )
+            }
+        )
+    }
+    componentDidUpdate(){
+        console.log("me actualicé")
     }
     render(){
+        console.log("estoy renderizando")
+        let contenido ;
+        if(this.state.ApiUsuario == ""){
+            contenido = <p>Cargando ...</p>
+        }else{
+            contenido = this.state.apiUsuario
+        }
      return(
     <div>
-        <tbody>
-            {this.state.productsList.map((users, index) => {
-                return <p></p>
-            })}
-        </tbody>
+        {contenido}
     </div>
   )
 }
 }
-export default ApiProducts
+export default ApiUsers
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,11 @@ import '../assets/css/Footer.css';
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
+import ApiUsers from "./ApiUsers";
 function App() {
   return (
     <React.StrictMode>
-    
+       <ApiUsers/>
       <Header/>
        <Body/>
        <Footer/>
