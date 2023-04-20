7a1d87b4d49846d50dae15ff8cdaddafb9ea8600
FrancoSanchezCamps <francosanchezcamps.100@gmail.com>
2023-04-20 01:14:23 -0300

    arreglo,

--- src/components/ApiProducts.jsx
+++ src/components/ApiProducts.jsx
@@ -1,69 +1,6 @@
 import React, {Component} from 'react';
 
 class ApiProducts extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            ApiProducts: []
-        }
-    }
-    
-    apiCall(){
-        
-        fetch("https:/catfact.ninja/fact")
-            .then(res => res.json())
-            .then(data => console.log(data.fact))
-          
-            /* .catch( e => console.log(e)) */
-            
-    }
-   
-    componentDidMount(){
-        console.log("me monté")
-        this.apiCall("http://localhost:3000/api/users",this.mostrarProducto)
-    }
-    mostrarProducto = (data) =>{
-        console.log(data)
-        this.setState(
-            {
-                ApiProducts: data.data.map((imprimir ,i) => imprimir = <p key={imprimir.first_name + i}></p>
-                )
-            }
-        )
-    }
-    componentDidUpdate(){
-        console.log("me actualicé")
-    }
-    render(){
-        console.log("estoy renderizando")
-        let contenido ;
-        if(this.state.ApiProducts == ""){
-            contenido = <p>Cargando ...</p>
-        }else{
-            contenido = this.state.ApiProducts
-        }
-     return(
-    <div>
-        {contenido} 
-        
-        <p>{this.state.data}</p>
-    </div>
-  )
-}
-}
-export default ApiProducts
-
-
-
-
-
-
-
-
-
-/* import React, {Component} from 'react';
-
-class ApiProducts extends Component {
     state ={
         productsList: [],
     }
@@ -87,4 +24,4 @@ class ApiProducts extends Component {
   )
 }
 }
-export default ApiProducts */ 
+export default ApiProducts
