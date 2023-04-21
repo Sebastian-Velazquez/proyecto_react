f6a78b78f73d2f69fec54b14470da7a98888dd5a
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-04-20 23:50:27 -0300

    separando en componentes el html

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import SideBar from './SideBar';
-import ContentWrapper from './ContentWrapper';
+import Logo from '../assets/images/logo-DH.png'
+import Jordan from '../assets/images/jordan-walke.png'
+import Mandalorian from '../assets/images/mandalorian.jpg'
+import Menu from './Menu'
+import Productos from './Productos';
+import Footer from './Footer';
+import Banner from './Banner';
+import Header from './Header';
+
 function App() {
   return (
     <React.Fragment>
-      	<div id="wrapper">
-          <SideBar />
-          <ContentWrapper />
-        </div>
+
+      <div id="wrapper">
+
+      <Menu/>
+
+		{/* Content Wrapper */}
+		<div id="content-wrapper" class="d-flex flex-column">
+
+			{/* Main Content */}
+			<div id="content">
+			<Header/>
+			<Banner/>
+
+			<Productos/>
+			</div>
+			{/* End of MainContent */}
+
+
+			<Footer/>
+
+		</div>
+		{/* End of Content Wrapper */}
+
+	</div>
+
+        
     </React.Fragment>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <React.StrictMode>
+        {/* Footer */}
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; Dashboard 2021</span>
+                </div>
+            </div>
+        </footer>
+        {/* End of Footer */}
+    </React.StrictMode>
+  )
+}
+
+export default Footer
